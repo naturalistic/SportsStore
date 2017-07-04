797ec7a97a83e4f05385212ab81592940352af77
Daniel Roberts <daniel.r.roberts@gmail.com>
2017-07-04 19:26:31 +1200

    First commit

---
+++ SportsStore/Controllers/ProductController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using SportsStore.Models;
+using SportsStore.Models.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SportsStore.Controllers
+{
+    public class ProductController : Controller
+    {
+        private IProductRepository repository;
+        public int PageSize = 4;
+
+        public ProductController(IProductRepository repo)
+        {
+            repository = repo;
+        }
+
+        public ViewResult List(string category, int page = 1) {
+            if (page < 1)
+            {
+                page = 1;
+            }
+            return View(new ProductsListViewModel {
+                Products = repository.Products
+                    .Where(p => p.Category == null || p.Category == category)
+                    .OrderBy(p => p.ProductID)
+                    .Skip((page - 1) * PageSize)
+                    .Take(PageSize),
+                PagingInfo = new PagingInfo
+                {
+                    CurrentPage = page,
+                    ItemsPerPage = PageSize,
+                    TotalItems = repository.Products.Count()
+                },
+                CurrentCategory = category
+             });
+        }
+    }
+}
